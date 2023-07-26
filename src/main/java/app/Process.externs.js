ea2ef51529b554e473b43f7a36adc470ae6876a7
faveoled <85760289+faveoled@users.noreply.github.com>
2023-07-26 18:27:16 +0300

    Update

--- src/main/java/app/Process.externs.js
+++ src/main/java/app/Process.externs.js
@@ -6,3 +6,11 @@ function process() {}
 process.hrtime = function(){};
 
 process.argv = function(){};
+
+process.env = function() {}
+
+process.exit = function(code) {}
+
+process.memoryUsage = function() {}
+
+process.uptime = function() {}
--- src/main/java/app/Process.java
+++ src/main/java/app/Process.java
@@ -1,6 +1,7 @@
 package app;
 
 import elemental2.core.JsArray;
+import elemental2.core.JsObject;
 import jsinterop.annotations.JsMethod;
 import jsinterop.annotations.JsProperty;
 import jsinterop.annotations.JsType;
@@ -17,5 +18,18 @@ public class Process {
 
     @JsMethod(name="hrtime")
     public static native JsArray<Integer> hrtime();
+
+    @JsProperty(name="env")
+    public static native JsObject getEnv();
+
+    @JsMethod(name="memoryUsage")
+    public static native JsObject memoryUsage();
+
+    @JsMethod(name="exit")
+    public static native void exit(int code);
+
+    @JsMethod(name="uptime")
+    public static native double uptime();
+
 
 }
